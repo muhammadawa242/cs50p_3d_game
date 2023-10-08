9d4c8c3bc8f53c01d11aa3661ad4686e44304f44
Awais Ahmad <muhammadawa242@gmail.com>
2023-10-09 03:36:21 +0500

    added fps_cam and wasd moves

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3, KeyboardButton, WindowProperties, loadPrcFile
 from screeninfo import get_monitors
+from FirstPersonCamera import FirstPersonCamera
 
 loadPrcFile('config.prc')   # see gloabal config variables page, also ConfigVariableManager.getGlobalPtr().listVariables()
 WIDTH, HEIGHT = get_monitors()[0].width, get_monitors()[0].height
@@ -16,6 +17,13 @@ class Game(ShowBase):
     def __init__(self):
         super(Game, ShowBase.__init__(self))
         self.win.requestProperties(properties)
+        
+        self.env = self.loader.loadModel('assets/grass.glb')
+        self.env.setPos(0,50,-10)
+        self.env.reparentTo(self.render)
+        
+        self.mouseLook = FirstPersonCamera(self, self.cam, self.render)
+        self.mouseLook.start()
         
         
 game = Game()
