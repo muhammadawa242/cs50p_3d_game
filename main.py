6d1214f703aabbac2f23584758ded3ebf7784756
Awais Ahmad <muhammadawa242@gmail.com>
2023-10-09 01:34:53 +0500

    main.py added with basic app structure to start building stuff upon

---
+++ main.py
@@ -0,0 +1,22 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3, KeyboardButton, WindowProperties, loadPrcFile
+from screeninfo import get_monitors
+
+loadPrcFile('config.prc')   # see gloabal config variables page, also ConfigVariableManager.getGlobalPtr().listVariables()
+WIDTH, HEIGHT = get_monitors()[0].width, get_monitors()[0].height
+
+# set fullscreen
+properties = WindowProperties()
+properties.setSize(WIDTH,HEIGHT)
+
+class Game(ShowBase):
+    def __init__(self):
+        super(Game, ShowBase.__init__(self))
+        self.win.requestProperties(properties)
+        
+        
+game = Game()
+game.run()
