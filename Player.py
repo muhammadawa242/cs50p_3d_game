b5157c24c7ac56f29de5fe27cbbbe5e66ec34fde
Awais Ahmad <muhammadawa242@gmail.com>
2023-10-27 00:16:30 +0500

    player shoots zombie

--- Player.py
+++ Player.py
@@ -3,20 +3,27 @@ from direct.showbase import DirectObject
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3, KeyboardButton, WindowProperties, loadPrcFile, MouseButton
+from direct.gui.OnscreenImage import OnscreenImage
+from panda3d.core import TransparencyAttrib
 
 class Player(DirectObject.DirectObject):
     def __init__(self, game):
         self.game = game
         self.gun = Actor('assets/pistol_bang.glb')
         self.gun.setScale(30)
-        self.game.taskMgr.add(self.gun_stuff,'reload')
+        self.game.taskMgr.add(self.gun_stuff,'gun_stuff')
         
         self.gun.reparentTo(self.game.render)
         self.gun.loop('Idle')
+        self.imageObject = OnscreenImage(image='assets/cross.png', pos=(0, 0, 0))
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        self.imageObject.setScale(.05)
         
     def gun_stuff(self,task):
-        self.gun.setHpr(self.game.cam, 180,0,-1)
-        self.gun.setPos(self.game.cam,0,10,2-50)
+        self.game.ray.setFromLens(self.game.camNode,0,0)
+        
+        self.gun.setHpr(self.game.cam, 188,-3,-1)
+        self.gun.setPos(self.game.cam,2.5,10,-47.5)#2-50)
 
         r = KeyboardButton.ascii_key('r')
         l = MouseButton.button(0)
@@ -29,5 +36,19 @@ class Player(DirectObject.DirectObject):
             
         if is_down(l):
             self.gun.play('Shoot')
+            if self.game.rayqueue.getNumEntries() > 0:
+                self.game.rayqueue.sortEntries()
+            
+                rayHit = self.game.rayqueue.getEntry(0)
+                rayHit_np = rayHit.getIntoNodePath()
+                name = rayHit_np.getName()
+                self.game.cTrav.removeCollider(rayHit_np)
+                # rayHit_np.removeNode()
+                # hitPos = rayHit.getSurfacePoint(render)
+                
+                # get the zombie/entry(0) to stop walking and animate death here
+                if rayHit_np.hasPythonTag(name):
+                    hitObject = rayHit_np.getPythonTag(name)
+                    hitObject.die = True
         
         return task.cont
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from FirstPersonCamera import FirstPersonCamera
 from Player import Player
 from Terrain import Terrain
 from Zombie import Zombie
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher, CollisionSphere, CollisionNode, CollisionRay, CollisionHandlerQueue
+import random
 
 # loadPrcFile('config.prc')   # see gloabal config variables page, also ConfigVariableManager.getGlobalPtr().listVariables()
 WIDTH, HEIGHT = get_monitors()[0].width, get_monitors()[0].height
@@ -26,10 +28,39 @@ class Game(ShowBase):
         self.mouseLook = FirstPersonCamera(self, self.cam, CAMERA_POS_Z, self.render)
         self.mouseLook.start()
         
+        self.cTrav = CollisionTraverser()
+        self.pusher = CollisionHandlerPusher()
+        self.rayqueue = CollisionHandlerQueue()
+        
+        self.ray_node = CollisionNode('ray')
+        self.ray = CollisionRay()
+        self.ray_node.addSolid(self.ray)
+        self.ray_np = self.cam.attachNewNode(self.ray_node)
+        
+        self.ray_np.node().setFromCollideMask(0x10)
+        self.ray_np.setCollideMask(0x1)
+        
         Player(self)
         Terrain(self)
-        zombie = Zombie(self)
-        zombie.get_zombie().loop('walk')
+        
+        self.cTrav.addCollider(self.ray_np, self.rayqueue)
+        self.ray_np.show()
+        
+        flinches = ['laflinch', 'rlflinch', 'llflinch', 'raflinch', 'flinchsmall', 'bigflinch']
+        for i in range(6):
+            z = Zombie(self, i, random.choice(flinches))
+            name = 'zombie'+str(i)
+            colliderNode = CollisionNode(name)
+            self.zombie = z.get_zombie()
+            
+            colliderNode.addSolid(CollisionSphere(0, 0, 40, 30))
+            colliderNode.setFromCollideMask(0x10)
+            collider = self.zombie.attachNewNode(colliderNode)
+            collider.setCollideMask(0x10)
+            collider.setPythonTag(name, z)
+            self.cTrav.addCollider(collider, self.pusher)
+            self.pusher.addCollider(collider, self.zombie)
+            # collider.show()
         
         self.camera.setPos(0,0,CAMERA_POS_Z)
         
